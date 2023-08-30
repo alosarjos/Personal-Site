b8fee1e84fa09f3054a83c18c2b98f59ae788d75
José Miguel Sarasola <alosarjos@gmail.com>
2023-08-30 18:08:45 +0200

    Update to Astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alosarjos.com',
+  site: "https://alosarjos.com",
   integrations: [tailwind(), sitemap()],
-  experimental: {
-    assets: true
-  }
 });
+
